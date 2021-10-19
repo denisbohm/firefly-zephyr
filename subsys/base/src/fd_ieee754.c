e1249a2bae91cb3a12813ab5271937d8fe579c41
Denis Bohm <denis@fireflydesign.com>
2021-10-19 10:31:26 -0500

    hide implementation functions

--- subsys/base/src/fd_ieee754.c
+++ subsys/base/src/fd_ieee754.c
@@ -74,10 +74,12 @@
 
 // Prototypes -----------------------------------------------------------------
 
+#if 0
 static int singles2halfp(void *target, void *source, int numel);
 static int doubles2halfp(void *target, void *source, int numel);
 static int halfp2singles(void *target, void *source, int numel);
 static int halfp2doubles(void *target, void *source, int numel);
+#endif
 
 //-----------------------------------------------------------------------------
 //
