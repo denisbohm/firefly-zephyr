a998d11931e627cb8b6a344839a4e02c1364365a
Denis Bohm <denis@fireflydesign.com>
2021-11-08 09:30:52 -0600

    add rtc

--- tools/macOS/Graphics Simulator/Graphics Simulator/fd_rtc.swift
+++ tools/macOS/Graphics Simulator/Graphics Simulator/fd_rtc.swift
@@ -7,8 +7,24 @@
 
 import Foundation
 
+// fd_rtc_initialize(void);
+@_cdecl("fd_rtc_initialize")
+func fd_rtc_initialize() {
+}
+
+// int64_t fd_rtc_get_utc(void);
+@_cdecl("fd_rtc_set_utc")
+func fd_rtc_set_utc(utc: UInt64) {
+}
+
+// bool fd_rtc_is_set(void);
+@_cdecl("fd_rtc_is_set")
+func fd_rtc_is_set() -> Bool {
+    return true
+}
+
 // int64_t fd_rtc_get_utc(void);
 @_cdecl("fd_rtc_get_utc")
-func fd_rtc_get_utc() -> Int64 {
-    return Int64(Date().timeIntervalSince1970)
+func fd_rtc_get_utc() -> UInt64 {
+    return UInt64(Date().timeIntervalSince1970)
 }
