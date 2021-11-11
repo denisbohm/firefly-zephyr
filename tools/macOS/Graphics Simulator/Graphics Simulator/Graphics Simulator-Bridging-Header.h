a4fec78f55afb1a4ac72ae2e20e75a2a18483d7c
Denis Bohm <denis@fireflydesign.com>
2021-11-11 15:41:19 -0600

    add empty log simulation

--- tools/macOS/Graphics Simulator/Graphics Simulator/Graphics Simulator-Bridging-Header.h
+++ tools/macOS/Graphics Simulator/Graphics Simulator/Graphics Simulator-Bridging-Header.h
@@ -3,14 +3,18 @@
 //
 
 #include "fd_assert.h"
+#include "fd_adc.h"
 #include "fd_delay.h"
 #include "fd_event.h"
 #include "fd_font_b612_regular_6.h"
 #include "fd_gpio.h"
 #include "fd_graphics.h"
 #include "fd_graphics_ssd1327.h"
+#include "fd_i2cm.h"
+#include "fd_log.h"
 #include "fd_ssd1327_bus.h"
 #include "fd_timer.h"
+#include "fd_uart.h"
 #include "fd_ux.h"
 #include "fd_ux_button.h"
 #include "fd_watch.h"
---
+++ tools/macOS/Graphics Simulator/Graphics Simulator/fd_log.swift
@@ -0,0 +1,29 @@
+//
+//  fd_log_macos.swift
+//  Graphics Simulator
+//
+//  Created by Denis Bohm on 10/12/21.
+//
+
+import Foundation
+
+class SimulateLog {
+    
+    init() {
+    }
+    
+}
+
+var simulateLog = SimulateLog()
+
+// void fd_log_initialize(void);
+@_cdecl("fd_log_initialize")
+func fd_log_initialize() {
+}
+
+// size_t fd_log_get(uint8_t *data, size_t size);
+
+@_cdecl("fd_log_get")
+func fd_log_get(data: UnsafePointer<UInt8>, size: size_t) -> size_t {
+    return 0
+}
