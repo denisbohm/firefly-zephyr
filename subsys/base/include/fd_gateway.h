af6891ee1bea05c32d2c50e793b572fbe16ad78d
Denis Bohm <denis@fireflydesign.com>
2021-11-30 12:02:13 -0600

    fix gateway add

--- subsys/base/include/fd_gateway.h
+++ subsys/base/include/fd_gateway.h
@@ -6,18 +6,17 @@
 
 #include <stddef.h>
 
-typedef uint8_t fd_gateway_identifier_t;
+typedef uint8_t fd_gateway_target_t;
 
 typedef bool (*fd_gateway_transmit_t)(const uint8_t *data, size_t size);
 
 typedef struct {
-    fd_gateway_identifier_t identifier;
     fd_gateway_transmit_t transmit;
 } fd_gateway_endpoint_t;
 
-void fd_gateway_initialize(fd_gateway_identifier_t identifier);
+void fd_gateway_initialize(fd_gateway_target_t target);
 
-void fd_gateway_add_endpoint(fd_gateway_endpoint_t *endpoint);
+void fd_gateway_add_endpoint(fd_gateway_target_t target, fd_gateway_endpoint_t endpoint);
 
 bool fd_gateway_process(fd_binary_t *message);
 
--- subsys/base/src/fd_gateway.c
+++ subsys/base/src/fd_gateway.c
@@ -6,45 +6,49 @@
 
 #include <string.h>
 
-#ifndef fd_gateway_endpoint_limit
-#define fd_gateway_endpoint_limit 5
+#ifndef fd_gateway_item_limit
+#define fd_gateway_item_limit 5
 #endif
 
 typedef struct {
-    uint32_t identifier;
-    fd_gateway_endpoint_t endpoints[fd_gateway_endpoint_limit];
-    uint32_t endpoint_count;
+    fd_gateway_target_t target;
+    fd_gateway_endpoint_t endpoint;
+} fd_gateway_item_t;
+
+typedef struct {
+    uint32_t target;
+    fd_gateway_item_t items[fd_gateway_item_limit];
+    uint32_t item_count;
 } fd_gateway_t;
 
 fd_gateway_t fd_gateway;
 
-fd_gateway_endpoint_t *fd_gateway_get_endpoint(fd_gateway_identifier_t identifier) {
-    for (uint32_t i = 0; i < fd_gateway.endpoint_count; ++i) {
-        fd_gateway_endpoint_t *endpoint = &fd_gateway.endpoints[i];
-        if (identifier == endpoint->identifier) {
-            return endpoint;
+fd_gateway_item_t *fd_gateway_get_item(fd_gateway_target_t target) {
+    for (uint32_t i = 0; i < fd_gateway.item_count; ++i) {
+        fd_gateway_item_t *item = &fd_gateway.items[i];
+        if (target == item->target) {
+            return item;
         }
     }
     return 0;
 }
 
-bool fd_gateway_is_endpoint(fd_gateway_identifier_t identifier) {
-    return fd_gateway_get_endpoint(identifier) != 0;
-}
-
-void fd_gateway_add_endpoint(fd_gateway_endpoint_t *endpoint) {
-    fd_assert(fd_gateway_is_endpoint(endpoint->identifier) == 0);
+void fd_gateway_add_endpoint(fd_gateway_target_t target, fd_gateway_endpoint_t endpoint) {
+    fd_assert(fd_gateway_get_item(target) == 0);
+    fd_gateway_item_t *item = &fd_gateway.items[fd_gateway.item_count++];
+    item->target = target;
+    item->endpoint = endpoint;
 }
 
 bool fd_gateway_transmit(fd_binary_t *message, fd_envelope_t *envelope) {
     if (!fd_envelope_encode(message, envelope)) {
         return false;
     }
-    fd_gateway_endpoint_t *endpoint = fd_gateway_get_endpoint(envelope->target);
-    if (endpoint == 0) {
+    fd_gateway_item_t *item = fd_gateway_get_item(envelope->target);
+    if (item == 0) {
         return false;
     }
-    return endpoint->transmit(message->buffer, message->put_index);
+    return item->endpoint.transmit(message->buffer, message->put_index);
 }
 
 bool fd_gateway_process(fd_binary_t *message) {
@@ -53,20 +57,20 @@ bool fd_gateway_process(fd_binary_t *message) {
         return false;
     }
 
-    if (envelope.target == fd_gateway.identifier) {
+    if (envelope.target == fd_gateway.target) {
         return fd_dispatch_process(message, &envelope);
     }
     
-    fd_gateway_endpoint_t *target = fd_gateway_get_endpoint(envelope.target);
-    if (target == 0) {
+    fd_gateway_item_t *item = fd_gateway_get_item(envelope.target);
+    if (item == 0) {
         return false;
     }
     fd_envelope_encode(message, &envelope);
-    return target->transmit(message->buffer, message->put_index);
+    return item->endpoint.transmit(message->buffer, message->put_index);
 }
 
-void fd_gateway_initialize(fd_gateway_identifier_t identifer) {
+void fd_gateway_initialize(fd_gateway_target_t target) {
     memset(&fd_gateway, 0, sizeof(fd_gateway));
 
-    fd_gateway.identifier = identifer;
+    fd_gateway.target = target;
 }
