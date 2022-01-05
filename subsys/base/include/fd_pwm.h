fdd088520d8a0d4ccc7845f223cc65319619bf28
Denis Bohm <denis@fireflydesign.com>
2022-01-05 13:41:59 -0600

    pwm interface tweaks

--- subsys/base/include/fd_pwm.h
+++ subsys/base/include/fd_pwm.h
@@ -6,9 +6,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef void (*fd_pwm_module_function_t)(void);
+
 typedef struct {
     uint32_t instance;
-    float frequency;
+    fd_pwm_module_function_t function;
 } fd_pwm_module_t;
 
 typedef enum {
@@ -16,20 +18,21 @@ typedef enum {
     fd_pwm_polarity_falling,
 } fd_pwm_polarity_t;
 
-typedef void (*fd_pwm_function_t)(bool on);
+typedef void (*fd_pwm_channel_function_t)(bool on);
 
 typedef struct {
     const fd_pwm_module_t *module;
     uint32_t instance;
-    fd_pwm_function_t function;
+    fd_pwm_channel_function_t function;
     fd_gpio_t gpio;
     fd_pwm_polarity_t polarity;
 } fd_pwm_channel_t;
 
 void fd_pwm_initialize(const fd_pwm_module_t *modules, uint32_t module_count, const fd_pwm_channel_t *channels, uint32_t channel_count);
 
-void fd_pwm_module_enable(const fd_pwm_module_t *module);
-void fd_pwm_module_disable(const fd_pwm_module_t *module);
+void fd_pwm_module_start(const fd_pwm_module_t *module, float frequency);
+bool fd_pwm_module_is_running(const fd_pwm_module_t *module);
+void fd_pwm_module_stop(const fd_pwm_module_t *module);
 
 void fd_pwm_channel_start(const fd_pwm_channel_t *channel, float duty_cycle);
 bool fd_pwm_channel_is_running(const fd_pwm_channel_t *channel);
--- subsys/base/src/fd_pwm_nrf5.c
+++ subsys/base/src/fd_pwm_nrf5.c
@@ -2,100 +2,143 @@
 
 #include "nrf.h"
 
+#include <zephyr.h>
+
 #include <math.h>
 #include <string.h>
 
 typedef struct {
-    float prescaler_divider;
-    uint32_t prescaler;
     uint32_t countertop;
     uint16_t sequence[4];
 } fd_pwm_module_state_t;
 
-fd_pwm_module_state_t fd_pwm_module_states[4];
+typedef struct {
+    const fd_pwm_module_t *modules;
+    uint32_t module_count;
+    const fd_pwm_channel_t *channels;
+    uint32_t channel_count;
 
-void fd_pwm_initialize(const fd_pwm_module_t *modules, uint32_t module_count, const fd_pwm_channel_t *channels __attribute__((unused)), uint32_t channel_count __attribute__((unused))) {
-    memset(fd_pwm_module_states, 0, sizeof(fd_pwm_module_states));
-    for (uint32_t i = 0; i < module_count; ++i) {
-        const fd_pwm_module_t *module = &modules[i];
-        fd_pwm_module_state_t *state = &fd_pwm_module_states[i];
+    fd_pwm_module_state_t states[4];
+} fd_pwm_t;
 
-        int prescaler_divider_int = (int)ceil(((16000000.0f / (32768.0f)) / module->frequency));
-        switch (prescaler_divider_int) {
-            case 1:
-                state->prescaler_divider = 1.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_1;
-                break;
-            case 2:
-                state->prescaler_divider = 2.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_2;
-                break;
-            case 4:
-                state->prescaler_divider = 4.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_4;
-                break;
-            case 8:
-                state->prescaler_divider = 8.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_8;
-                break;
-            case 16:
-                state->prescaler_divider = 16.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_16;
-                break;
-            case 32:
-                state->prescaler_divider = 32.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_32;
-                break;
-            case 64:
-                state->prescaler_divider = 64.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_64;
-                break;
-            default:
-                state->prescaler_divider = 128.0;
-                state->prescaler = PWM_PRESCALER_PRESCALER_DIV_128;
-                break;
-        }
-        // countertop is 15-bits, so with prescaler divider of 128 the minimum frequency is 16000000/(128*32767) is ~3.82 Hz
-        state->countertop = (uint32_t)(16000000.0f / (state->prescaler_divider * module->frequency));
-    }
+fd_pwm_t fd_pwm;
+
+int fd_pwm_handler(int index) {
+    fd_pwm.modules[index].function();
+    return 0;
+}
+
+ISR_DIRECT_DECLARE(fd_pwm_pwm0_handler) {
+    return fd_pwm_handler(0);
+}
+
+ISR_DIRECT_DECLARE(fd_pwm_pwm1_handler) {
+    return fd_pwm_handler(1);
+}
+
+ISR_DIRECT_DECLARE(fd_pwm_pwm2_handler) {
+    return fd_pwm_handler(2);
+}
+
+ISR_DIRECT_DECLARE(fd_pwm_pwm3_handler) {
+    return fd_pwm_handler(3);
+}
+
+void fd_pwm_initialize(const fd_pwm_module_t *modules, uint32_t module_count, const fd_pwm_channel_t *channels, uint32_t channel_count) {
+    memset(&fd_pwm, 0, sizeof(fd_pwm));
+
+    IRQ_DIRECT_CONNECT(PWM0_IRQn, 0, fd_pwm_pwm0_handler, 0);
+    irq_enable(PWM0_IRQn);
+
+    IRQ_DIRECT_CONNECT(PWM1_IRQn, 0, fd_pwm_pwm1_handler, 0);
+    irq_enable(PWM1_IRQn);
+
+    IRQ_DIRECT_CONNECT(PWM2_IRQn, 0, fd_pwm_pwm2_handler, 0);
+    irq_enable(PWM2_IRQn);
+
+    IRQ_DIRECT_CONNECT(PWM3_IRQn, 0, fd_pwm_pwm3_handler, 0);
+    irq_enable(PWM3_IRQn);
 }
 
 static
 fd_pwm_module_state_t *fd_pwm_get_state(uint32_t instance) {
 #ifdef NRF52_SERIES
     if (instance == (uint32_t)NRF_PWM0) {
-        return &fd_pwm_module_states[0];
+        return &fd_pwm.states[0];
     }
     if (instance == (uint32_t)NRF_PWM1) {
-        return &fd_pwm_module_states[1];
+        return &fd_pwm.states[1];
     }
     if (instance == (uint32_t)NRF_PWM2) {
-        return &fd_pwm_module_states[2];
+        return &fd_pwm.states[2];
     }
     if (instance == (uint32_t)NRF_PWM3) {
-        return &fd_pwm_module_states[3];
+        return &fd_pwm.states[3];
     }
 #endif
 #ifdef NRF53_SERIES
     if (instance == (uint32_t)NRF_PWM0_S) {
-        return &fd_pwm_module_states[0];
+        return &fd_pwm.states[0];
     }
     if (instance == (uint32_t)NRF_PWM1_S) {
-        return &fd_pwm_module_states[1];
+        return &fd_pwm.states[1];
     }
     if (instance == (uint32_t)NRF_PWM2_S) {
-        return &fd_pwm_module_states[2];
+        return &fd_pwm.states[2];
+    }
+    if (instance == (uint32_t)NRF_PWM3_S) {
+        return &fd_pwm.states[3];
     }
 #endif
     return 0;
 }
 
-void fd_pwm_module_enable(const fd_pwm_module_t *module) {
-    NRF_PWM_Type *pwm = (NRF_PWM_Type *)module->instance;
+void fd_pwm_module_start(const fd_pwm_module_t *module, float frequency) {
+    float prescaler_divider;
+    uint32_t prescaler;
+    int prescaler_divider_int = (int)ceil(((16000000.0f / (32768.0f)) / frequency));
+    switch (prescaler_divider_int) {
+        case 1:
+            prescaler_divider = 1.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_1;
+            break;
+        case 2:
+            prescaler_divider = 2.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_2;
+            break;
+        case 4:
+            prescaler_divider = 4.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_4;
+            break;
+        case 8:
+            prescaler_divider = 8.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_8;
+            break;
+        case 16:
+            prescaler_divider = 16.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_16;
+            break;
+        case 32:
+            prescaler_divider = 32.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_32;
+            break;
+        case 64:
+            prescaler_divider = 64.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_64;
+            break;
+        default:
+            prescaler_divider = 128.0;
+            prescaler = PWM_PRESCALER_PRESCALER_DIV_128;
+            break;
+    }
+    // countertop is 15-bits, so with prescaler divider of 128 the minimum frequency is 16000000/(128*32767) is ~3.82 Hz
     fd_pwm_module_state_t *state = fd_pwm_get_state(module->instance);
+    state->countertop = (uint32_t)(16000000.0f / (prescaler_divider * frequency));
+
+    NRF_PWM_Type *pwm = (NRF_PWM_Type *)module->instance;
     pwm->ENABLE = PWM_ENABLE_ENABLE_Enabled << PWM_ENABLE_ENABLE_Pos;
     pwm->MODE = PWM_MODE_UPDOWN_Up << PWM_MODE_UPDOWN_Pos;
-    pwm->PRESCALER = state->prescaler << PWM_PRESCALER_PRESCALER_Pos;
+    pwm->PRESCALER = prescaler << PWM_PRESCALER_PRESCALER_Pos;
     pwm->COUNTERTOP = state->countertop << PWM_COUNTERTOP_COUNTERTOP_Pos;
     pwm->LOOP = PWM_LOOP_CNT_Disabled << PWM_LOOP_CNT_Pos;
     pwm->DECODER = (PWM_DECODER_LOAD_Individual << PWM_DECODER_LOAD_Pos) | (PWM_DECODER_MODE_RefreshCount << PWM_DECODER_MODE_Pos);
@@ -103,10 +146,18 @@ void fd_pwm_module_enable(const fd_pwm_module_t *module) {
     pwm->SEQ[0].CNT = ((sizeof(state->sequence) / sizeof(uint16_t)) << PWM_SEQ_CNT_CNT_Pos);
     pwm->SEQ[0].REFRESH = 0;
     pwm->SEQ[0].ENDDELAY = 0;
+    if (module->function) {
+        pwm->INTEN = PWM_INTEN_PWMPERIODEND_Msk;
+    }
     pwm->TASKS_SEQSTART[0] = 1;
 }
 
-void fd_pwm_module_disable(const fd_pwm_module_t *module) {
+bool fd_pwm_module_is_running(const fd_pwm_module_t *module) {
+    NRF_PWM_Type *pwm = (NRF_PWM_Type *)module->instance;
+    return (pwm->ENABLE & PWM_ENABLE_ENABLE_Msk) != 0;
+}
+
+void fd_pwm_module_stop(const fd_pwm_module_t *module) {
     NRF_PWM_Type *pwm = (NRF_PWM_Type *)module->instance;
     pwm->EVENTS_STOPPED = 0;
     pwm->TASKS_STOP = 1;
