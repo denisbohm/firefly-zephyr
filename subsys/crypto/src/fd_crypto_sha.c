a4c060419c1f6d1b17eca7505dcadb49fed0eb1e
Denis Bohm <denis@fireflydesign.com>
2022-02-02 18:46:54 -0600

    sha compile fixes

--- subsys/crypto/src/fd_crypto_sha.c
+++ subsys/crypto/src/fd_crypto_sha.c
@@ -2,7 +2,7 @@
 
 #include "sha.h"
 
-void fd_crypto_sha_initialize(void *context) {
+bool fd_crypto_sha_initialize(void *context) {
     SHA_CTX *ctx = (SHA_CTX *)context;
     SHA1_Init(ctx);
     return true;
@@ -10,11 +10,11 @@ void fd_crypto_sha_initialize(void *context) {
 
 bool fd_crypto_sha_update(void *context, const uint8_t *data, uint32_t length) {
     SHA_CTX *ctx = (SHA_CTX *)context;
-    SHA1_Update(ctx, data, length);
+    SHA1_Update(ctx, (uint8_t *)data, length);
     return true;
 }
 
-void fd_crypto_sha_finalize(uint8_t *hash) {
+bool fd_crypto_sha_finalize(void *context, uint8_t *hash) {
     SHA_CTX *ctx = (SHA_CTX *)context;
     SHA1_Final(hash, ctx);
     return true;
