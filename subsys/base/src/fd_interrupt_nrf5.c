9ed9e9907479a8f6f43531b65ebb1b5cfd40eabc
Denis Bohm <denis@fireflydesign.com>
2022-02-25 08:30:25 -0600

    interrupt wait

--- subsys/base/src/fd_interrupt_nrf5.c
+++ subsys/base/src/fd_interrupt_nrf5.c
@@ -5,18 +5,18 @@
 #include <stdint.h>
 #include <string.h>
 
-uint32_t im_interrupt_disable(void) {
+uint32_t fd_interrupt_disable(void) {
     uint32_t state = __get_PRIMASK();
     __disable_irq();
     return state;
 }
 
-void im_interrupt_enable(uint32_t state) {
+void fd_interrupt_enable(uint32_t state) {
     if (!state) {
         __enable_irq();
     }
 }
 
-void im_interrupt_wait(void) {
+void fd_interrupt_wait(void) {
     __WFI();
 }
